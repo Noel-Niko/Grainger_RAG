58dfbf9d323f31aa4590d9e2a36f6157d5200a99
Noel-Niko <83922762+Noel-Niko@users.noreply.github.com>
2024-05-27 16:14:01 -0400

    Script updates

--- start.sh
+++ start.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-# Initialize Conda for bash and zsh shells
-echo "source /opt/conda/etc/profile.d/conda.sh" >> ~/.bashrc
-echo "conda init bash" >> ~/.bashrc
-echo "conda init zsh" >> ~/.zshrc
+# Source the shell profile to apply Conda environment
+source ~/.bashrc
+source ~/.zshrc
+
+
+# After activating the Conda environment
+echo "Active Conda environment:"
+conda env list
+echo "Python path:"
+which python
 
 # Activate the Conda environment
 conda activate myenv
-#
-## After activating the Conda environment
-#echo "Active Conda environment:"
-#conda env list
-#echo "Python path:"
-#which python
-#
-## Source the shell profile to apply Conda environment
-#source ~/.bashrc
-#source ~/.zshrc
-#
-## Install required packages
-#echo "Install conda packages"
-#conda install -y -c conda-forge transformers
-#conda install -y -c pytorch pytorch torchvision torchaudio cpuonly
-#conda install -y streamlit
-#conda install -y -c conda-forge faiss-cpu
-#conda install -y pandas
-#conda install -y numpy
-#conda install -y ChatGPT
+
+
+# Install required packages
+echo "Install conda packages"
+conda install -y -c conda-forge transformers
+conda install -y -c pytorch pytorch torchvision torchaudio cpuonly
+conda install -y streamlit
+conda install -y -c conda-forge faiss-cpu
+conda install -y pandas
+conda install -y numpy
+conda install -y ChatGPT
 
 # Navigate to the directory containing scripts and data
 cd /app || { echo "Failed to change directory to /app"; exit 1; }

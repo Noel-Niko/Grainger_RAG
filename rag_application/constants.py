8b5f2f1421a741a02ae0368258f6ba2102d3d065
Noel-Niko <nnosse@wgu.edu>
2024-05-31 10:36:16 -0400

    ReadME update

--- rag_application/constants.py
+++ rag_application/constants.py
@@ -1,8 +1,7 @@
 # TODO: Store using secure vaults.
 from langchain_core.prompts import ChatPromptTemplate
 
-langchainApiKey = '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'
-chatOpenAiKey = 'sk-proj-F7ow6Bu53kHPKaQDYbA0T3BlbkFJ2ykR1JffM2jA6Tg0qPOH'
+chatOpenAiKey = '{PLACE HERE}'
 initial_question_wrapper = ('I was asked the following question but only have access to data about products and their '
                             'descriptive characteristics. Refine the question to a text string form suitable for '
                             'searching my products faiss vector index. Add up to 10 additional key words that are '

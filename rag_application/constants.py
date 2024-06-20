8cd2580a411f7da3cf2c3a8d49c217cf30b751be
Noel-Niko <83922762+Noel-Niko@users.noreply.github.com>
2024-06-20 19:03:04 -0400

    Prompt development

--- rag_application/constants.py
+++ rag_application/constants.py
@@ -7,10 +7,10 @@ langchainApiKey = os.getenv('LANGCHAIN_API_KEY')
 chatOpenAiKey = os.getenv('CHAT_OPENAI_KEY')
 email = os.getenv('EMAIL')
 
-initial_question_wrapper = ('You are a chat-bot answering product queries. ALWAYS include the product_id with the '
-                            'product title in your answers. Refine the question to a text string of just the product '
-                            'title which may be a short sentence, or if not a product title, then the keywords such '
-                            'as the nouns and adjectives.  QUESTION:')
+initial_question_wrapper = ('You are a chat-bot answering product queries. ALWAYS include the Product ID: {value} '
+                            'with the product title in your answers. Refine the question to a text string of just '
+                            'the product title which may be a short sentence, or if not a product title, then the '
+                            'keywords such as the nouns and adjectives.  QUESTION:')
 prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context but 
 provide as much information as you can:
 
